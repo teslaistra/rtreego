e8fba09b96269674a6e8ee907db629364b0861b8
teslaistra <danyefimoff@gmail.com>
2020-07-29 12:59:43 +0300

    stress test for line NN

--- test/test.go
+++ test/test.go
@@ -22,16 +22,15 @@ func test1(rt rtreego.Rtree) {
 	rt.Insert(p1)
 	rt.Insert(p2)
 	rt.Insert(out)
-	result := rt.NnInRadiusLine(100, 120, *arbat)
-	fmt.Println(rt.NnInRadiusLine(100, 120, *arbat))
-
-	rt.Delete(result[2])
-	fmt.Println(rt.NnInRadiusLine(100, 120, *arbat))
+	fmt.Println(rt.NnInRadiusLine(100, 120, *arbat, ""))
+	fmt.Println("____________sort________________")
+	//	rt.Delete(result[2])
+	fmt.Println(rt.NnInRadiusLine(100, 120, *arbat, "sort"))
 
 }
-func stress(rt rtreego.Rtree) {
+func stressPoints(rt rtreego.Rtree) {
 
-	num := 1000000
+	num := 10000000
 	fmt.Println("Загружаю", num, "точек")
 	start := time.Now()
 
@@ -61,18 +60,62 @@ func stress(rt rtreego.Rtree) {
 	}
 }
 
+func stressLines(rt rtreego.Rtree) {
+
+	num := 100000
+	fmt.Println("Inserting", num, "lines")
+	start := time.Now()
+
+	for i := 0; i < num; i++ {
+		latA := randFloat(55.765654, 55.755966)
+		lonA := randFloat(37.643501, 37.583127)
+		latB := randFloat(55.765654, 55.755966)
+		lonB := randFloat(37.643501, 37.583127)
+		line, _ := rtreego.NewLine(rtreego.Point{latA, lonA}, rtreego.Point{latB, lonB}, "")
+		rt.Insert(line)
+	}
+	elapsed := time.Since(start)
+	fmt.Println("Inserting took", elapsed)
+	maxDist := 60000
+
+	for i := 0; i <= maxDist; i += 5000 {
+		start := time.Now()
+		latA := randFloat(55.765654, 55.755966)
+		lonA := randFloat(37.643501, 37.583127)
+		latB := randFloat(55.765654, 55.755966)
+		lonB := randFloat(37.643501, 37.583127)
+		line, _ := rtreego.NewLine(rtreego.Point{latA, lonA}, rtreego.Point{latB, lonB}, "")
+		fmt.Println(rt.NnInRadiusLine(10, float64(i), *line, "sort"))
+		elapsed := time.Since(start)
+		fmt.Println("Searching in radius", i, "took via Sort", elapsed)
+
+		start = time.Now()
+		fmt.Println(rt.NnInRadiusLine(10, float64(i), *line, ""))
+		fmt.Println("Searching in radius", i, "took via Quicksort", time.Since(start))
+		fmt.Println("At line", latA, lonA, "  --  ", latB, lonB)
+		fmt.Println("_________________________________________________________")
+
+	}
+}
 func main() {
 
-	rt := rtreego.NewTree(10, 20)
+	//rt := rtreego.NewTree(10, 20)
 
-	p1 := rtreego.NewPoint(55.752612, 37.581785)
-	p2 := rtreego.NewPoint(55.752575, 37.580905)
+	//p1 := rtreego.NewPoint(55.752612, 37.581785)
+	//p2 := rtreego.NewPoint(55.752575, 37.580905)
 
-	rt.Insert(p1)
-	rt.Insert(p2)
+	//rt.Insert(p1)
+	//rt.Insert(p2)
 
-	fmt.Println(rt.NnInRadiusPoint(1, 2000, *rtreego.NewPoint(55.752588, 37.578526), "sort"))
-	stress(*rt)
+	//fmt.Println(rt.NnInRadiusPoint(1, 2000, *rtreego.NewPoint(55.752588, 37.578526), "sort"))
+	//test1(*rt)
+	//stress(*rt)
+	//stressLines(*rt)
+	from, _ := rtreego.NewLine(rtreego.Point{55.687898, 37.525656}, rtreego.Point{55.685431, 37.520978}, "")
+	to, _ := rtreego.NewLine(rtreego.Point{55.688026, 37.520583}, rtreego.Point{55.684284, 37.526417}, "")
+
+	fmt.Println(rtreego.DistanceLineToLine(*from, *to))
+
 }
 
 func randFloat(min, max float64) float64 {
